db75e675c4e576034cad7c62b8c1b65f688da1f6
Lukas Vanelderen <lukas.vanelderen@cern.ch>
2013-10-28 19:21:30 +0100

    first working version of setup scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,48 +1,76 @@
-# some switches
-cmsswdir=$HOME                 # where to keep the CMSSW release ?
-scratch=$MYSCRATCH             # where is my scratch space ?
-
-# read command line arguments
-projectdir=$HOME/$1
-if [ -z "$projectdir" ]; then
-   echo "error: first command line argument: name of project directory to be created"
+# check environment
+if [ -z $MYSCRATCH ]; then
+   echo "ERROR: please define a variable \$MYSCRATCH"
    return
 fi
+
+# some switches
+project="VBF-LS-tau"
+cmsswdir=$MYSCRATCH/$project-CMSSW                 # where to keep the CMSSW release ?
+scratchdir=$MYSCRATCH/$project             # where is my scratch space ?
+projectdir=$HOME/$project
 
 # where are we?
 pwd=$PWD
 
-
 # check if project directory is free
 if [ -d $projectdir ]; then
-   echo "ERROR: directory $HOME/$projectdir already exists"
+   echo "ERROR: project directory $projectdir already exists"
+   return
+fi
+# check if scratch directory is free
+if [ -d $scratchdir ]; then
+   echo "ERROR: scratch directory $scratchdir already exists"
+   return
+fi
+# check if cmssw directory is free
+if [ -d $cmsswdir ]; then
+   echo "ERROR: cmssw directory $cmsswdir already exists"
    return
 fi
 
-
 # setup cmssw area
-echo "\n SETUP CMSSW \n"
+echo "SETUP CMSSW"
+mkdir $cmsswdir
 cd $cmsswdir
 ini cmssw
-source $pwd/setup_cmssw.sh
+source $pwd/setup_cmssw_light.sh
 cd $pwd
 
 # get anascripts
-echo "\n RETRIEVING ANASCRIPTS \n"
-export $ANASCRIPTS=$HOME/anascripts
+echo "SETUP ANASCRIPTS"
+export ANASCRIPTS=$HOME/anascripts
 if [ ! -d $ANASCRIPTS ]; then
+    echo "   RETRIEVING ANASCRIPTS"
     git clone https://github.com/lveldere/anascripts $HOME/anascripts
+else
+    echo "   ANASCRIPTS ALREADY AVAILABLE,"
+    echo "   CONSIDER TO PULL IN THE LATEST VERSION THROUGH GIT"
+fi
 export PATH=$PATH:$ANASCRIPTS
 
 # create project directory
-mkmyproject.py $projectdir $scratch
+echo "SETUP PROJECT DIRECTORY"
+mkmyproject.py $projectdir $scratchdir
 
 # setup analyzers
+echo "SETUP ANALYZER DIRECTORY"
 git clone https://github.com/rathjd/VBF-LS-tau-analyzer $projectdir/analyzers
 
 # setup tools
+echo "SETUP TOOLS"
 git clone https://github.com/lveldere/VBF-LS-tau-tools $projectdir/tools
 
 # retrieve sample lists
+echo "SETUP SAMPLE LIST"
+git clone https://github.com/lveldere/VBF-LS-tau-data $projectdir/data/VBF-LS-tau-data
+rmdir $projectdir/data/samples
+ln -s $projectdir/data/VBF-LS-tau-data/samples $projectdir/data/samples
 
 # complete the setenv script
+echo "export PATH=\$PATH:\$PROJECTDIR/tools/drawHist:\$PROJECTDIR/tools/scripts" &> $projectdir/setenv.sh
+echo "export PYTHONPATH=\$PYTHONPATH:\$PROJECTDIR/tools/drawHist" &> $projectdir/setenv.sh
+
+echo "PLEASE START FROM NEW SHELL AND RUN:"
+echo "\$ source $projectdir/setenv.sh"
+ 
---
+++ setup/setup_cmssw_light.sh
@@ -0,0 +1,4 @@
+export SCRAM_ARCH=slc5_amd64_gcc462
+scramv1 project CMSSW CMSSW_5_3_7_patch4
+cd CMSSW_5_3_7_patch4/src
+cmsenv
