732b2a9067e44c45f139b04d2fa3203d4c63855e
Daniele Marconi <daniele.marconi87@gmail.com>
2014-11-14 15:58:34 +0100

    Bug fixes

--- setup/setup_cmssw_ntuplemaker.sh
+++ setup/setup_cmssw_ntuplemaker.sh
@@ -12,11 +12,9 @@ cmsenv
 
 git cms-addpkg CondFormats/JetMETObjects
 git cms-addpkg PhysicsTools/PatAlgos
-git cms-merge-topic vadler:53X-addMVAElectronId
 git cms-addpkg PhysicsTools/PatUtils
-git cms-merge-topic -u vadler:53X-tagset133511-newBTagging
-git cms-merge-topic -u vadler:53X-tagset133511-newEGIsolation
-git cms-merge-topic -u cms-tau-pog:CMSSW_5_3_X_HighPt
+git cms-merge-topic -u cms-tau-pog:CMSSW_5_3_X_boostedTaus_2013Dec17
+git cms-merge-topic -u TaiSakuma:53X-met-130910-01
 cd $CMSSW_BASE/src/
 git clone https://github.com/Sam-Harper/usercode SHarper
 cd SHarper
@@ -27,11 +25,10 @@ cd CMGTools
 git clone https://github.com/h2gglobe/External External
 cd ../
 cd $CMSSW_BASE/src/
-git cms-merge-topic -u TaiSakuma:53X-met-130910-01
 git clone https://github.com/CMSRA2Tau/HighMassAnalysis HighMassAnalysis
 cd $CMSSW_BASE/src/JetMETCorrections/Type1MET/python/
 rm -v pfMETsysShiftCorrections_cfi.py
-wget http://dl.dropbox.com/u/206488/CMS/RA2TAU/VBF-re-PATTuples/pfMETsysShiftCorrections_cfi.py
+wget --no-check-certificate http://dl.dropbox.com/u/206488/CMS/RA2TAU/VBF-re-PATTuples/pfMETsysShiftCorrections_cfi.py
 scram b clean
 scram build -c
 scram b -j 8
